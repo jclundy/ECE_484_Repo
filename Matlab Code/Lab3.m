aa449e0cf94d0b60fac2e715e79366999a9dc86e
Joseph Lundy <joseph.lundy456@gmail.com>
2017-11-09 16:44:25 -0500

    plots lead controller - discrete and controller for part a of the lab

--- Matlab Code/Lab3.m
+++ Matlab Code/Lab3.m
@@ -25,30 +25,41 @@ K2 = 0.062;
 K3 = 4.78;
 %% Outer loop controller
 % define controller
-C_2 = Kp_c2 * (s + z_c2) / (s + z_c2);
+C_2 = Kp_c2 * (s + z_c2) / (s + p_c2);
 % define the plant
 G_2 = K2 * K3 / s^2;
 P_aug = G * G_2;
 G_outer = C_2 * P_aug / (1 + C_2 + P_aug);
 
+%% Discrete Controller
+C_D2 = c2d(C_2,T,'tustin')
+[NUM, DEN] = tfdata(C_D2, 'v');
+format long
+NUM
+format long
+DEN
+
+
+
 %% Graphing Results
 sim('Lab3_Model_Continuous'); % continuous model
-% sim('Lab3_Model_Discrete');
+sim('Lab3_Model');
 figure();
 % plot(ball_out); hold on;
-plot(ball_out_cont);
-plot(input_sig);
-
+plot(ball_out); hold on;
+plot(filtered_contol); hold on;
+plot(ref_sig2);
 
 title("System Response - Discrete Controller");
 xlabel("Time (sec)");
 ylabel("Ball Position (m)");
-legend("Discrete", "Continuous", "Input");
+legend("Ball Position", "Input (for inner loop)", "Reference");
 
-% figure();
-% plot(theta_out_cont); hold on;
-% plot(input_sig_cont);
-% title("System Response - Continuous Controller");
-% xlabel("Time (sec)");
-% ylabel("Ball Position  (m)");
-% legend("Output", "Input");
+figure();
+plot(ball_out_cont); hold on;
+plot(filtered_contol_cont); hold on;
+plot(ref_sig2);
+title("System Response - Continuous Controller");
+xlabel("Time (sec)");
+ylabel("Ball Position  (m)");
+legend("Ball Position", "Input (for inner loop)", "Reference");
