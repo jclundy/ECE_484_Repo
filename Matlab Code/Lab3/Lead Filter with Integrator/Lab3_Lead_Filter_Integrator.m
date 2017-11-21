a3fe27cb4c23ee91cebc719cdf10dd4a50f3fca1
Joseph Lundy <joseph.lundy456@gmail.com>
2017-11-21 13:22:31 -0500

    experiment with lead controller with integrator

--- Matlab Code/Lab3/Lead Filter with Integrator/Lab3_Lead_Filter_Integrator.m
+++ Matlab Code/Lab3/Lead Filter with Integrator/Lab3_Lead_Filter_Integrator.m
@@ -9,32 +9,30 @@ K1 = 0.66538; %1.53
 tau = 0.030541; %0.025
 %% Inner loop controller
 % define controller
-C = Kp*(s+z)/(s+p);
+C_inner = Kp*(s+z)/(s+p);
 % define the plant
-P = K1/(tau*s^2 + s);
+P_inner = K1/(tau*s^2 + s);
 % system response
-G = P*C/(1+P*C);
-fb = bandwidth(G) * 0.5 / pi();
+G_inner = P_inner*C_inner/(1+P_inner*C_inner);
+fb = bandwidth(G_inner) * 0.5 / pi();
 ts_recommended = 1/ (25 * fb);
 % define inner loop digital controller
-C_D = c2d(C,T,'tustin');
+C_D = c2d(C_inner,T,'tustin');
 %% Plant Model
 K2 = 0.062;
 K3 = 4.78;
-G_2 = K2 * K3 / s^2;
+P_outer = K2 * K3 / s^2;
 %% Outer loop parameters
 p_c2 = 2.5; 
 z_c2 = 0.35;
 Kp_c2 = 7;
 %% Outer loop controller
-C_2 = Kp_c2*(s + z_c2) / (s + p_c2);
-% P_aug = G * G_2;
-% G_outer = C_2 * P_aug / (1 + C_2 + P_aug);
+C_outer = Kp_c2*(s + z_c2) / (s + p_c2) * 1/s;
+P_aug = G_inner * P_outer;
+G_outer = C_outer * P_aug / (1 + C_outer + P_aug);
 % digital controller
-C_D2 = c2d(C_2,T,'tustin')
-a = C_D2.Numerator{1}(1); b =C_D2.Numerator{1}(2); 
-c = C_D2.Denominator{1}(1); d= C_D2.Denominator{1}(2);
-
+C_D2 = c2d(C_outer,T,'tustin')
+return
 %% Graphing Results
 sim('Lead_Filter_Integrator_Continuous_a');
 sim('Lead_Filter_Integrator_Digital_a');
